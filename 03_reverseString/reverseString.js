38b3073cc9825c2da14b3d86882e9956b6380ce3
Razvan Ionescu <44871305+razvanionescu26@users.noreply.github.com>
2025-03-12 23:12:56 +0200

    03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = "";
+    for (let char=string.length-1; char>=0; char--) {
+        reversedString += string[char];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
