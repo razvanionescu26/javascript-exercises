f7668170a5403ea2a849bb4199bc85d3d4b1fe72
Razvan Ionescu <44871305+razvanionescu26@users.noreply.github.com>
2025-03-16 18:09:26 +0200

    04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
 const removeFromArray = function() {
+    const args = Array.from(arguments);
+    let array = args[0];
+    for (let it = 0; it < array.length; it++) {
+        for (let elem = 1; elem < args.length; elem++)
+        if (array[it] === args[elem]) {
+            array.splice(it,1);
+            it--;
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
