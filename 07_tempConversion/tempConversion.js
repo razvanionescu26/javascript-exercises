6b0bb38f0e0d09ea908afae903ed0fc967d529e7
Razvan Ionescu <44871305+razvanionescu26@users.noreply.github.com>
2025-03-16 18:39:56 +0200

    07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  let converted = (value - 32) * 5/9;
+  return +converted.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  let converted = (value * 9/5 + 32);
+  return +converted.toFixed(1);
 };
 
 // Do not edit below this line
