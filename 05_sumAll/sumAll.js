4c541bfd374b95739e2a809e75d67c5ac072629c
Razvan Ionescu <44871305+razvanionescu26@users.noreply.github.com>
2025-03-16 18:24:52 +0200

    05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    let sum = 0;
+    if (min < 0 || max < 0 || !Number.isFinite(min) || !Number.isFinite(max) 
+        || !Number.isInteger(min) || !Number.isInteger(max)) {
+        return "ERROR";
+    }
+    if (min > max) {
+        let aux = min;
+        min = max;
+        max = aux;
+    }
+    for (let it = min; it <= max; it++) {
+        sum += it;
+    }
+    return sum;
 };
 
 // Do not edit below this line
